3b1d34eea5fed601a31c89d796cc6c0e11e74fde
RajendraGupta <rajendra.gupta16@gmail.com>
2020-06-29 19:48:59 +0530

    Add additional Script

--- sys.databases.sql
+++ sys.databases.sql
@@ -1,3 +1,8 @@
 Select * from sys.databases where name not in('tempdb','master',',model','msdb');
 Go
 sp_helpdb
+GO
+SELECT 
+  SERVERPROPERTY('BuildClrVersion') AS BuildClrVersion,
+  SERVERPROPERTY('ProductLevel') AS ProductLevel,
+  SERVERPROPERTY('ProductVersion') AS ProductVersion; 

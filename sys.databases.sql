e3660a15a9645e0fe45399053e0c244fa109cd3d
RajendraGupta <rajendra.gupta16@gmail.com>
2020-07-02 14:44:08 +0530

    bc
    dddd

--- sys.databases.sql
+++ sys.databases.sql
@@ -1,7 +1,3 @@
-Select * from sys.databases where name not in('tempdb','master',',model','msdb');
-Go
-sp_helpdb
-GO
 SELECT 
   SERVERPROPERTY('BuildClrVersion') AS BuildClrVersion,
   SERVERPROPERTY('ProductLevel') AS ProductLevel,

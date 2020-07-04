19476dc3af754ce380bc9f8c456ae8839fb45975
RajendraGupta <rajendra.gupta16@gmail.com>
2020-07-04 13:12:12 +0530

    move item from stage area to local rep

--- SERVERPROPERTY Resource DB.sql
+++ SERVERPROPERTY Resource DB.sql
@@ -2,6 +2,6 @@ SELECT
 SERVERPROPERTY('servername'),
 SERVERPROPERTY('ProductVersion ') AS ProductVersion,  
 SERVERPROPERTY('ProductLevel') AS ProductLevel,  
-SERVERPROPERTY('ResourceVersion') AS ResourceVersion,  
+  
 SERVERPROPERTY('ResourceLastUpdateDateTime') AS ResourceLastUpdateDateTime,  
 SERVERPROPERTY('Collation') AS Collation; 
